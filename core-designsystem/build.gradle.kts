9be40095fa3fe4bd3dea10ab2b76e5aea60f9e27
vini <vinnygarciadelgado@gmail.com>
2022-07-18 19:45:08 -0300

    Feature Leaderboards

---
+++ core-designsystem/build.gradle.kts
@@ -0,0 +1,55 @@
+import com.vini.lorproject.buildsrc.Configs
+import com.vini.lorproject.buildsrc.Dependencies
+import com.vini.lorproject.buildsrc.Versions
+
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    compileSdk = Configs.compileSdkVersion
+
+    defaultConfig {
+        minSdk = Configs.minSdkVersion
+        targetSdk = Configs.targetSdkVersion
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    buildFeatures {
+        compose = true
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = Versions.kotlinCompilerExtensionVersion
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+    implementation(Dependencies.coreKtx)
+    implementation(Dependencies.composeDefault)
+    implementation(Dependencies.composeTooling)
+    implementation(Dependencies.composeActivity)
+    implementation(Dependencies.composeFundation)
+    implementation(Dependencies.composeMaterial)
+    implementation(Dependencies.appCompat)
+    implementation(Dependencies.material)
+    implementation(Dependencies.material3)
+}

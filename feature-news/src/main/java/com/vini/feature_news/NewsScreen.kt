09f157db929b086d7f7573f7973fff10e910723f
vini <vinnygarciadelgado@gmail.com>
2022-08-08 18:56:29 -0300

    Feature News

--- feature-news/src/main/java/com/vini/feature_news/NewsScreen.kt
+++ feature-news/src/main/java/com/vini/feature_news/NewsScreen.kt
@@ -1,12 +1,11 @@
 package com.vini.feature_news
 
+import androidx.compose.animation.*
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyListScope
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.collectAsState
@@ -14,7 +13,8 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
@@ -63,29 +63,55 @@ fun NewsScreen(
     }
 }
 
+@OptIn(ExperimentalAnimationApi::class)
 @Composable
 fun NewsContent(viewModel: NewsViewModel = hiltViewModel()) {
     val metaDecksUIState by viewModel.state.collectAsState()
-
-    when (val uiState = metaDecksUIState) {
-        is NewsState.Loading -> {
-            Column(
-                modifier = Modifier.fillMaxSize(),
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                LorLoadingWheel(modifier = Modifier.background(Color.Transparent))
+    AnimatedContent(targetState = metaDecksUIState,
+        transitionSpec = {
+            fadeIn() + slideInVertically(animationSpec = tween(400),
+                initialOffsetY = { fullHeight -> fullHeight }) with
+                    fadeOut(animationSpec = tween(200))
+        }) { targetState ->
+        when (targetState) {
+            is NewsState.Loading -> {
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    LorLoadingWheel(modifier = Modifier.background(Color.Transparent))
+                }
             }
-//            LeaderboardsLoading()
-        }
-        is NewsState.Error -> {
-//            LeaderboardsError(error = uiState.error)
-        }
-        is NewsState.NewsList -> {
-            NewsList(news = uiState.news)
-        }
-        else -> {
+            is NewsState.Error -> {
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(
+                        text = targetState.error,
+                        style = MaterialTheme.typography.titleLarge,
+                        fontWeight = FontWeight.SemiBold,
+                        textAlign = TextAlign.Center,
+                        color = Color(0xFFF2F0F0)
+                    )
+                    Spacer(modifier = Modifier.height(16.dp))
+                    Button(onClick = { /*TODO*/ }, colors = ButtonDefaults.buttonColors(containerColor = Color(0xFFD13739))) {
+                        Text(
+                            text = "Tentar novamente",
+                            style = MaterialTheme.typography.labelLarge,
+                            fontWeight = FontWeight.W700,
+                        )
+                    }
+                }
+            }
+            is NewsState.NewsList -> {
+                NewsList(news = targetState.news)
+            }
+            else -> {
 //            LeaderboardsEmpty()
+            }
         }
     }
 }
